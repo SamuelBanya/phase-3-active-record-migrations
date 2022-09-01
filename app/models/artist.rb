54bd8a9d915083e083c070b14d6221835eed8f61
Samuel Banya <sbanya@fastmail.com>
2022-09-01 17:53:35 -0400

    commit tree e20677aa5e026c8277635d07fcc0c9f0c18a93c5

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,5 @@
-# add the Artist class here
+# NOTE: This is the 'singular' Artist class for the 'artists' (plural) table
+# This also just serves to inherit all of the methods from the 'ActiveRecord::Base' class as well
+class Artist < ActiveRecord::Base
+
+end
